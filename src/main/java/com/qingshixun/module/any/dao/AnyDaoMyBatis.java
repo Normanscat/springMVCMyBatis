44d67a0fb8bf38542b5d5b452ff7cead237e7f7c
Normanscat <31616367+Normanscat@users.noreply.github.com>
2017-11-10 11:10:04 +0800

    Update AnyDaoMyBatis.java

--- src/main/java/com/qingshixun/module/any/dao/AnyDaoMyBatis.java
+++ src/main/java/com/qingshixun/module/any/dao/AnyDaoMyBatis.java
@@ -1,9 +1,3 @@
-/*****************************************************************************
- * Copyright (c) 2016, www.qingshixun.com
- *
- * All rights reserved
- *
-*****************************************************************************/
 package com.qingshixun.module.any.dao;
 
 import com.qingshixun.core.MyBatisRepository;
