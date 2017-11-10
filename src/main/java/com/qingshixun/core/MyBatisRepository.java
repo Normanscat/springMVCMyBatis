64624e9e6b503de511898cf2c67b0abe9fad69b6
Normanscat <31616367+Normanscat@users.noreply.github.com>
2017-11-10 11:10:57 +0800

    Update MyBatisRepository.java

--- src/main/java/com/qingshixun/core/MyBatisRepository.java
+++ src/main/java/com/qingshixun/core/MyBatisRepository.java
@@ -1,9 +1,3 @@
-/*****************************************************************************
- * Copyright (c) 2016, www.qingshixun.com
- *
- * All rights reserved
- *
-*****************************************************************************/
 package com.qingshixun.core;
 
 import org.springframework.stereotype.Component;
