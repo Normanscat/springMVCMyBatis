cb057ccb57aebc9f66e41be7ce24cda4b53eaf3d
Norman <Norman@DESKTOP-H1CGTEL>
2017-11-10 10:50:19 +0800

    mybatis

---
+++ src/main/java/com/qingshixun/core/MyBatisRepository.java
@@ -0,0 +1,14 @@
+/*****************************************************************************
+ * Copyright (c) 2016, www.qingshixun.com
+ *
+ * All rights reserved
+ *
+*****************************************************************************/
+package com.qingshixun.core;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public @interface MyBatisRepository {
+
+}
---
+++ src/main/java/com/qingshixun/module/any/dao/AnyDaoMyBatis.java
@@ -0,0 +1,14 @@
+/*****************************************************************************
+ * Copyright (c) 2016, www.qingshixun.com
+ *
+ * All rights reserved
+ *
+*****************************************************************************/
+package com.qingshixun.module.any.dao;
+
+import com.qingshixun.core.MyBatisRepository;
+
+@MyBatisRepository
+public interface AnyDaoMyBatis {
+
+}
